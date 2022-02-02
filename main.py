936579f393570909767917871ee7bd813de49482
qaziok <72215548+qaziok@users.noreply.github.com>
2022-02-02 02:25:12 +0100

    Adding and removing points

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from math import sqrt
 import numpy
 import cv2
 
-
 class Punkty(list):
     def add(self,Punkt,waga=0):
         if len(self):
@@ -20,6 +19,20 @@ class Punkty(list):
         else:
             self.append(Punkt)
 
+    def delete(self,Coords):
+        for p in self:
+            odl = sqrt((p[0] - Coords[0]) ** 2 + (p[1] - Coords[1]) ** 2)
+            if odl < 10:
+                self.remove(p)
+                break
+
+    def check(self,Coords):
+        for p in self:
+            odl = sqrt((p[0] - Coords[0]) ** 2 + (p[1] - Coords[1]) ** 2)
+            if odl < 10:
+                return True
+        return False
+
     def normalize(self):
         odl = 5
         for point1 in self:
@@ -28,34 +41,45 @@ class Punkty(list):
                     point2[0] = point1[0]
                 if point1[1] - odl < point2[1] < point1[1] + odl:
                     point2[1] = point1[1]
+
     def scale(self,scale):
         for p in self:
             p[0] = scale*p[0]
             p[1] = scale*p[1]
 
+    def draw(self,image):
+        for p in self:
+            pint = (int(p[0]), int(p[1]))
+            cv2.circle(image, pint, 2, (1, 0, 0), -1)
+            cv2.circle(image, pint, 2, (1, 0, 0), -1)
+            cv2.circle(image, pint, 5, (0, 1, 0))
+            cv2.circle(image, pint, 5, (0, 1, 0))
+            cv2.circle(image, pint, 10, (1, 0, 1))
+            cv2.circle(image, pint, 10, (1, 0, 1))
+
+    def desmos(self):
+        for p in self:
+            print(f'({int(p[0])},{600-int(p[1])})',end=',')
+        print()
+
+
+
+
 if __name__ == '__main__':
-#for c in range(ord('A'),ord('Z')+1):
     punkty = Punkty()
     letter = numpy.zeros((600, 800), dtype="uint8")
-    sign = 'W'
-    #cv2.putText(letter,sign,(50,480),cv2.FONT_HERSHEY_PLAIN, 40, (255, 255, 255),40)
+    sign = 'B'
     cv2.putText(letter, sign, (5, 590), cv2.FONT_HERSHEY_DUPLEX, 27, (255, 255, 255), 1)
-    #letter = cv2.GaussianBlur(letter,(3,3),0)
-    (T, letter) = cv2.threshold(letter, 200, 255, cv2.THRESH_BINARY)
-    #letter = cv2.Canny(letter, 50, 240)
-    #letter = cv2.resize(letter, (512, 512))
     canny = np.float32(letter)
     dst = cv2.cornerHarris(canny, 5, 3, 0.02)
     ret, dst = cv2.threshold(dst, 0.1 * dst.max(), 255, 0)
     dst = np.uint8(dst)
-    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
     corners = cv2.cornerSubPix(canny, np.float32(centroids), (5, 5), (-1, -1), criteria)
     rgb = cv2.cvtColor(canny, cv2.COLOR_GRAY2RGB)
     for i in range(1, len(corners)):
-        #cv2.putText(rgb,str(i),(int(corners[i][0]),int(corners[i][1])),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255),2)
         punkty.add([corners[i][0],corners[i][1]])
-
     rgb[dst > 0.1 * dst.max()] = [0,0,255]
 
     rho = 1  # distance resolution in pixels of the Hough grid
@@ -63,30 +87,38 @@ if __name__ == '__main__':
     threshold = 20  # minimum number of votes (intersections in Hough grid cell)
     min_line_length = 30  # minimum number of pixels making up a line
     max_line_gap = 15  # maximum gap in pixels between connectable line segments
-    line_image = np.copy(rgb) * 0  # creating a blank to draw lines on
 
-    # Run Hough on edge detected image
-    # Output "lines" is an array containing endpoints of detected line segments
-    lines = cv2.HoughLinesP(letter, rho, theta, threshold, np.array([]),
-                            min_line_length, max_line_gap)
+    lines = cv2.HoughLinesP(letter, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
     if lines is not None:
         for line in lines:
             for x1, y1, x2, y2 in line:
                 punkty.add([x1,y1])
                 punkty.add([x2,y2])
-                #cv2.line(rgb, (x1, y1), (x2, y2), (1, 0, hm), 1)
+                #cv2.line(rgb,(x1,y1),(x2,y2),(1,0,0),2)
+
     punkty.normalize()
     punkty.scale(1)
-    for i,p in enumerate(punkty):
-        cv2.circle(rgb, (int(p[0]),int(p[1])), 2, (1, 0, 0), -1)
-        cv2.circle(rgb, (int(p[0]),int(p[1])), 2, (1, 0, 0), -1)
-        cv2.circle(rgb, (int(p[0]),int(p[1])), 5, (0, 1, 0))
-        cv2.circle(rgb, (int(p[0]),int(p[1])), 5, (0, 1, 0))
-        cv2.circle(rgb, (int(p[0]),int(p[1])), 10, (1, 0, 1))
-        cv2.circle(rgb, (int(p[0]),int(p[1])), 10, (1, 0, 1))
-        #cv2.putText(rgb,str(i),(int(p[0]),int(p[1])),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,1),1)
-    cv2.imshow('image', rgb)
+    #punkty.desmos()
+    bbg = rgb.copy()
+    punkty.draw(bbg)
+    cv2.imshow('image', bbg)
+
+
+    def mouse_event(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            bbg = rgb.copy()
+            punkty.add([x,y])
+            punkty.normalize()
+            punkty.draw(bbg)
+            cv2.imshow('image', bbg)
+
+        if event == cv2.EVENT_RBUTTONDOWN:
+            bbg = rgb.copy()
+            punkty.delete([x,y])
+            punkty.normalize()
+            punkty.draw(bbg)
+            cv2.imshow('image', bbg)
+
+    cv2.setMouseCallback('image', mouse_event)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-#najwyżej trzeba iść po białych pixelach i pogrupować je bliskością i stosunkiem współrzędnych i z tego wyznaczyć min i max
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
